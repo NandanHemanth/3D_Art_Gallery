06bf41bd46386692bcd482ee1c73203171ba6a65
Nandan H <theuniverseisflat101@gmail.com>
2023-10-04 20:05:54 +0530

    threejs file addition

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "./modules/three.js";
 import { scene, setupScene } from "./modules/scene.js";
 import { createPaintings } from "./modules/paintings.js";
 import { createWalls } from "./modules/walls.js";
